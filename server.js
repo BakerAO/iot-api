e7ea0c9206cbc27b1769e5231836236adcc9c9ff
BakerAO <dpshunter@gmail.com>
2019-08-07 23:15:59 +0000

    Up

--- server.js
+++ server.js
@@ -1,32 +1,56 @@
 const express = require('express');
-const server = express();
+const app = express();
+const https = require('https');
+const fs = require('fs');
 const mysql = require('mysql');
 require('dotenv').config();
 
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "innov8.host");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.innov8.host/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/api.innov8.host/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/api.innov8.host/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+app.get("/", (req, res) => {
+	res.send("Hi");
 });
 
-server.get("/temperatures", (req, res) => {
-    const connection = mysql.createConnection({
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: 'iot',
+// app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+//app.use(function(req, res, next) {
+  //  res.header("Access-Control-Allow-Origin", "innov8.host");
+   // res.header("Access-Control-Allow-Headers", "*");
+   // next();
+//});
+
+//app.get("/temperatures", (req, res) => {
+  //  const connection = mysql.createConnection({
+    //    host: process.env.MYSQL_HOST,
+      //  port: process.env.MYSQL_PORT,
+       // user: process.env.MYSQL_USER,
+       // password: process.env.MYSQL_PASSWORD,
+       // database: 'iot',
         // ssl: {
         //     ca: fs.readFileSync(__dirname + '/ca.crt')
         // }
-    });
+   // });
 
-    const query = `SELECT * FROM temperatures`;
-    connection.query(query, (err, rows, fields) => {
-        res.json(rows)
-    });
-});
+    //const query = `SELECT * FROM temperatures`;
+    //connection.query(query, (err, rows, fields) => {
+      //  res.json(rows)
+   // });
+//});
 
-server.listen(80, () => {
-    console.log("Started")
+const server = https.createServer(credentials, app);
+
+server.listen(443, () => {
+	console.log('Started');
 });
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80);
