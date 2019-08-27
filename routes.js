d02a6af243d558096c7c9e2a55917db661c1d709
BakerAO <dpshunter@gmail.com>
2019-08-27 09:20:26 -0500

    up

--- routes.js
+++ routes.js
@@ -1,55 +1,58 @@
 const router = require('express').Router();
 const mysql = require('mysql');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'iot',
+     // ssl: {
+     //     ca: fs.readFileSync(__dirname + '/ca.crt')
+     // }
+});
 
 router.get('/', (req, res) => {
 	res.send('Hi');
 });
 
 router.get("/temperatures", (req, res) => {
-    const connection = mysql.createConnection({
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: 'iot',
-         // ssl: {
-         //     ca: fs.readFileSync(__dirname + '/ca.crt')
-         // }
-    });
-
      const query = `SELECT * FROM temperatures`;
      connection.query(query, (err, rows, fields) => {
         res.json(rows)
     });
 });
 
-router.post('/register', (req, res) => {
-    const connection = mysql.createConnection({
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: 'iot',
-         // ssl: {
-         //     ca: fs.readFileSync(__dirname + '/ca.crt')
-         // }
-    });
-
+router.post('/register', async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.user_password, salt);
+    const date = Date.now();
     const query = `
         INSERT INTO users (user_email, user_password, user_created)
-        VALUES (${req.body.user_email}, ${req.body.user_password}, ${'date'});
+        VALUES ('${req.body.user_email}', '${hashPassword}', '${date}');
     `;
-
     connection.query(query, (err, rows, fields) => {
         if (err) {
             console.log(err);
             res.status(400).send(err);
         } else {
-            console.log('User created');
             res.sendStatus(200);
         }
     });
+});
+
+router.post('/login', (req, res) => {
+    const query = `SELECT * FROM users
+        WHERE user_email = '${req.body.user_email}'
+    `;
+    const userEmail = connection.query(query, (err) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            return
+        }
+    })
 });
 
 module.exports = router;
