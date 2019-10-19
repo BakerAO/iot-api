22e3ffafc2a1eb5596670789b3ee58298e788d52
BakerAO <dpshunter@gmail.com>
2019-10-19 10:52:23 -0500

    Update

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
+const privateKey = 'secretKey';
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
@@ -13,15 +15,29 @@ const connection = mysql.createConnection({
      // }
 });
 
+function extractToken(req, res, next) {
+    const bearerHeader = req.headers['Authorization'];
+    if (typeof bearerHeader !== undefined) {
+        const bearer = bearerHeader.split(' ');
+        const token = bearer[1];
+        req.token = token;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+// Routes
 router.get('/', (req, res) => {
 	res.send('Hi');
 });
 
-router.get("/temperatures", (req, res) => {
-     const query = `SELECT * FROM temperatures`;
-     connection.query(query, (err, rows, fields) => {
-        res.json(rows)
-    });
+router.get("/temperatures", extractToken, (req, res) => {
+    jwt.verify(req.token, );
+    //  const query = `SELECT * FROM temperatures`;
+    //  connection.query(query, (err, rows, fields) => {
+    //     res.json(rows)
+    // });
 });
 
 router.post('/register', async (req, res) => {
@@ -43,24 +59,33 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-    const query = `
-        SELECT * FROM users
-        WHERE user_email = '${req.body.user_email}'
-    `;
+    const user = {
+        user_id: 111,
+        user_email: req.body.user_email
+    };
 
-    connection.query(query, async (err, rows, fields) => {
-        if (err) {
-            res.status(400).send(err);
-        } else {
-            const validPassword = await bcrypt.compare(req.body.user_password, rows[0].user_password);
-            console.log(validPassword)
-            if (!validPassword) {
-                res.send('You idiot')
-            } else {
-                res.send('Nice')
-            }
-        }
+    jwt.sign({ user: user }, privateKey, (err, token) => {
+        res.json({ token: token });
     });
+
+    // const query = `
+    //     SELECT * FROM users
+    //     WHERE user_email = '${req.body.user_email}'
+    // `;
+
+    // connection.query(query, async (err, rows, fields) => {
+    //     if (err) {
+    //         res.status(400).send(err);
+    //     } else {
+    //         const validPassword = await bcrypt.compare(req.body.user_password, rows[0].user_password);
+    //         console.log(validPassword)
+    //         if (!validPassword) {
+    //             res.send('You failed')
+    //         } else {
+    //             res.send('Nice')
+    //         }
+    //     }
+    // });
 });
 
 module.exports = router;
