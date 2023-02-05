00b855e6021ded64ef9d2d16db0f32c2075e65a3
BakerAO <dpshunter@gmail.com>
2023-02-05 15:47:13 -0600

    Update

--- src/dataSources/index.js
+++ src/dataSources/index.js
@@ -1,7 +1,5 @@
 import mysqlPoolI from './mysqlPool.js'
-import ddbClientI from './ddbClient.js'
 import mqttClientI from './mqttClient.js'
 
 export const mysqlPool = mysqlPoolI
-export const ddbClient = ddbClientI
 export const mqttClient = mqttClientI
